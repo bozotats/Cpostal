f2a86a4338b835ad0ecc86c4decca4fed11ae054
bozotats <112152266+bozotats@users.noreply.github.com>
2024-08-16 16:00:00 -0700

    chequeo de error para no argumentos

--- EncontrarCodigo.java
+++ EncontrarCodigo.java
@@ -1,7 +1,14 @@
 import java.io.*;
+import java.lang.System.*;
 
 public class EncontrarCodigo {
     public static void main(String[] args) throws Exception {
+        
+        if (args.length == 0) {
+            System.out.println("Error: no se introducio ningun argumento, vuelva a intentar");
+            System.exit(1);
+        }
+        
         File csv = new File("./codigos_postales.csv");
         BufferedReader br = new BufferedReader(new FileReader(csv));
         String currentline;
